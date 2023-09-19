2ba3ae34890c45143629601133e537afc4eb5146
ysy56 <ysy561356@naver.com>
2023-09-19 12:39:12 +0900

    feat : use dom, save HTML object

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,19 @@
 "use strict";
 
+const id = document.querySelector('#id'),
+    pw = document.querySelector('#pw'),
+    loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    };
+
+    console.log(req);
+};
+
+console.log(id.value);
 console.log('hello');
