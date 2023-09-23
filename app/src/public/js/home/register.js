fe493b7b6dd40e4f3958fd536296663707abf3b6
ysy56 <ysy561356@naver.com>
2023-09-23 21:53:15 +0900

    feat : check id & pw

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,14 @@ const id = document.querySelector('#id'),
 registerBtn.addEventListener('click', register);
 
 function register() {
+
+    if (!id.value) return alert('아이디를 입력해주십시오.');
+    if (pw !== confirmPw) return alert('비밀번호가 일치하지 않습니다.');
+    
     const req = {
         id: id.value,
         name: name.value,
         pw: pw.value,
-        confirmPw: confirmPw.value
     };
 
     // 요청 또는 전달
